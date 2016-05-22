neomake/neomake

commit c4fdb2cd40d9369461d5bd1febf42257690931b9

neomake#utils#Exists: just use Vim's executable function

autoload/neomake/utils.vim
@@ -95,15 +95,7 @@ function! neomake#utils#DevNull()
 endfunction
 
 function! neomake#utils#Exists(exe) abort
-    if neomake#utils#IsRunningWindows()
-        " TODO: Apparently XP uses a different utility to where, see
-        " https://github.com/benekastah/neomake/issues/19#issuecomment-65195452
-        let cmd = 'where'
-    else
-        let cmd = 'which'
-    endif
-    call system(cmd.' '.shellescape(a:exe))
-    return !v:shell_error
+    return executable(exe)
 endfunction
 
 function! neomake#utils#Random() abort

commit 8e132707748a137a6e3957bf3ee95c2dc528603f

Fix neomake#utils#Exists stub
Broken in c4fdb2c.

autoload/neomake/utils.vim
@@ -96,7 +96,7 @@ endfunction
 
 function! neomake#utils#Exists(exe) abort
     " DEPRECATED: just use executable() directly.
-    return executable(exe)
+    return executable(a:exe)
 endfunction
 
 function! neomake#utils#Random() abort