neomake/neomake

commit 5ac6c8022f8f296f9e2052e1c35e1f57a51cee4a

fix go checks cwd problems, and use go test instead of go build for the main syntax checker. Fixes #358

autoload/neomake/makers/ft/go.vim
@@ -4,13 +4,19 @@ function! neomake#makers#ft#go#EnabledMakers()
     return ['go', 'golint', 'govet']
 endfunction
 
+" The mapexprs in these are needed because cwd will make the command print out
+" the wrong path (it will just be ./%:h in the output), so the mapexpr turns
+" that back into the relative path
+
 function! neomake#makers#ft#go#go()
     return {
         \ 'args': [
-            \ 'build',
-            \ '-o', neomake#utils#DevNull()
+            \ 'test', '-c',
+            \ '-o', neomake#utils#DevNull(),
         \ ],
         \ 'append_file': 0,
+        \ 'cwd': '%:h',
+        \ 'mapexpr': 'expand("%:h") . "/" . v:val',
         \ 'errorformat':
             \ '%W%f:%l: warning: %m,' .
             \ '%E%f:%l:%c:%m,' .
@@ -22,6 +28,8 @@ endfunction
 
 function! neomake#makers#ft#go#golint()
     return {
+        \ 'cwd': '%:h',
+        \ 'mapexpr': 'expand("%:h") . "/" . v:val',
         \ 'errorformat':
             \ '%f:%l:%c: %m,' .
             \ '%-G%.%#'
@@ -31,9 +39,10 @@ endfunction
 function! neomake#makers#ft#go#govet()
     return {
         \ 'exe': 'go',
-        \ 'cwd': '%:p:h',
         \ 'args': ['vet'],
         \ 'append_file': 0,
+        \ 'cwd': '%:h',
+        \ 'mapexpr': 'expand("%:h") . "/" . v:val',
         \ 'errorformat':
             \ '%Evet: %.%\+: %f:%l:%c: %m,' .
             \ '%W%f:%l: %m,' .

commit 19284b36a8ce6590e014ccd608b4520cc3abfc6a

go: Fix golint maker
Wrongfully broken in 5ac6c80, golint behaves differently in contrast to the core go toolchain

autoload/neomake/makers/ft/go.vim
@@ -28,8 +28,6 @@ endfunction
 
 function! neomake#makers#ft#go#golint()
     return {
-        \ 'cwd': '%:h',
-        \ 'mapexpr': 'expand("%:h") . "/" . v:val',
         \ 'errorformat':
             \ '%f:%l:%c: %m,' .
             \ '%-G%.%#'