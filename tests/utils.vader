neomake/neomake

commit 0768f431fe774615e9c2b994dfffbfd70ff1bb85

neomake#utils#LogMessage: lazily build msg

autoload/neomake/utils.vim
@@ -4,8 +4,8 @@ scriptencoding utf-8
 function! neomake#utils#LogMessage(level, msg) abort
     let verbose = get(g:, 'neomake_verbose', 1)
     let logfile = get(g:, 'neomake_logfile')
-    let msg ='Neomake: '.a:msg
     if verbose >= a:level
+        let msg ='Neomake: '.a:msg  " .' ('.a:level.')'
         if a:level ==# 0
             echohl ErrorMsg
         endif

commit ab22f656cd3ce77a7092568c412b7422c15889e8

Fix neomake#utils#LogMessage when logfile is used
Fixes https://github.com/neomake/neomake/issues/519.
Regressed in 0768f43.

autoload/neomake/utils.vim
@@ -4,8 +4,8 @@ scriptencoding utf-8
 function! neomake#utils#LogMessage(level, msg) abort
     let verbose = get(g:, 'neomake_verbose', 1)
     let logfile = get(g:, 'neomake_logfile')
+    let msg ='Neomake: '.a:msg  " .' ('.a:level.')'
     if verbose >= a:level
-        let msg ='Neomake: '.a:msg  " .' ('.a:level.')'
         if a:level ==# 0
             echohl ErrorMsg
         endif

tests/utils.vader
@@ -1,0 +1,9 @@
+Include: _setup.vader
+
+Execute (neomake#utils#LogMessage writes to logfile always):
+  let neomake_verbose = 0
+  Save neomake_logfile
+  let g:neomake_logfile = tempname()
+  call neomake#utils#LogMessage(1, 'msg1')
+  Assert readfile(g:neomake_logfile)[0] =~# 'Log level 1: Neomake: msg1$'
+  Restore