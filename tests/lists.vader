neomake/neomake

commit 07ec33274fff05d786fc85d1fc64bb71054a9473

s:create_locqf_list: use setqflist/setloclist
This is more straight-forward than having to (re)set the errorformat.
It will also not trigger QuickFixCmd{Pre,Post}.

tests: simplify busting quickfix/loclist cleanup/cache busting.

autoload/neomake.vim
@@ -1065,20 +1065,11 @@ function! s:create_locqf_list(jobinfo) abort
     call neomake#utils#DebugMessage(printf(
                 \ 'Creating %s list.',
                 \ file_mode ? 'location' : 'quickfix'), a:jobinfo)
-    " Empty the quickfix/location list (using a valid 'errorformat' setting).
-    let save_efm = &errorformat
-    let &errorformat = '%-G'
-    try
-        if file_mode
-            lgetexpr ''
-        else
-            cgetexpr ''
-        endif
-    finally
-        let &errorformat = save_efm
-    endtry
-    " TODO: correct?!
-    call s:HandleLoclistQflistDisplay(a:jobinfo.file_mode)
+    if file_mode
+        call setloclist(0, [])
+    else
+        call setqflist([])
+    endif
 endfunction
 
 function! s:clean_for_new_make(jobinfo) abort

tests/include/setup.vader
@@ -5,15 +5,8 @@ Before:
   let g:neomake_test_log_all_messages = 1
   let g:neomake_test_messages_last_idx = -1
 
-  " Work around https://github.com/vim/vim/issues/1676.
-  if get(g:, 'neomake_test_cache_bust', [0])[0]
-    let bust_name = 'bust_cache_'.g:neomake_test_cache_bust[1]
-    lgetexpr bust_name.':1676:workaround_vim_issue'
-    exe 'bwipe' bust_name
-    let g:neomake_test_cache_bust = [0, g:neomake_test_cache_bust[1]+1]
-  endif
-  noautocmd lgetexpr ''
-  noautocmd cgetexpr ''
+  call setloclist(0, [])
+  call setqflist([])
 
   " One-time setup.
   if exists(':NeomakeTestsWaitForFinishedJobs') != 2
@@ -24,12 +17,16 @@ Before:
     " Work around https://github.com/vim/vim/issues/1676.
     if has('patch-7.4.2017')
       let g:neomake_test_cache_bust = [0, 0]
-      augroup neomake_test_cache_bust
-        au!
-        au QuickFixCmdPre * let g:neomake_test_cache_bust[0] = 1
-      augroup END
     endif
   endif
 
 After:
   NeomakeTestsGlobalAfter
+
+  " Work around https://github.com/vim/vim/issues/1676.
+  if has('patch-7.4.2017')
+    let bust_name = 'bust_cache_'.g:neomake_test_cache_bust[1]
+    lgetexpr bust_name.':1676:workaround_vim_issue'
+    exe 'bwipe' bust_name
+    let g:neomake_test_cache_bust = [0, g:neomake_test_cache_bust[1]+1]
+  endif

commit 7e876aa2a7386d7b8608e2252481d492aa8598cd

Fix closing of empty location/quickfix lists
Fixes https://github.com/neomake/neomake/issues/122#issuecomment-303892087.
Regressed in 07ec3327.

Without patch-7.4.379 it could create a new list to work around
`:lwindow` opening it for invalid entries, but that seemed to be too
much of a workaround.
In the end we would probably filter the list ourselves and use
:lopen/:copen instead.

autoload/neomake.vim
@@ -783,10 +783,10 @@ function! s:HandleLoclistQflistDisplay(jobinfo) abort
         return
     endif
     if a:jobinfo.file_mode
-        call neomake#utils#DebugMessage('Opening location list.')
+        call neomake#utils#DebugMessage('Handling location list: executing lwindow.')
         let cmd = 'lwindow'
     else
-        call neomake#utils#DebugMessage('Opening quickfix list.')
+        call neomake#utils#DebugMessage('Handling quickfix list: executing cwindow.')
         let cmd = 'cwindow'
     endif
     if open_val == 2
@@ -1126,6 +1126,8 @@ function! s:CleanJobinfo(jobinfo) abort
             endif
         endif
 
+        call s:HandleLoclistQflistDisplay(a:jobinfo)
+
         call neomake#utils#hook('NeomakeFinished', {'jobinfo': a:jobinfo})
     endif
 
@@ -1260,6 +1262,13 @@ function! s:cd_to_jobs_cwd(jobinfo) abort
     return ['', cwd, '']
 endfunction
 
+" Do we need to replace (instead of append) the location/quickfix list, for
+" :lwindow to not open it with only invalid entries?!
+" Without patch-7.4.379 this does not work though, and a new list needs to
+" be created (which is not done).
+let s:needs_to_replace_qf_for_lwindow = has('patch-7.4.379')
+            \ && (!has('patch-7.4.1647') || (has('nvim') && !has('nvim-0.2.0')))
+
 function! s:ProcessEntries(jobinfo, entries, ...) abort
     if s:need_to_postpone_loclist(a:jobinfo)
         return s:queue_action('WinEnter', ['s:ProcessEntries',
@@ -1297,9 +1306,17 @@ function! s:ProcessEntries(jobinfo, entries, ...) abort
         endif
         try
             if file_mode
-                call setloclist(0, a:entries, 'a')
+                if s:needs_to_replace_qf_for_lwindow
+                    call setloclist(0, getloclist(0) + a:entries, 'r')
+                else
+                    call setloclist(0, a:entries, 'a')
+                endif
             else
-                call setqflist(a:entries, 'a')
+                if s:needs_to_replace_qf_for_lwindow
+                    call setqflist(getqflist() + a:entries, 'r')
+                else
+                    call setqflist(a:entries, 'a')
+                endif
             endif
         finally
             if empty(cd_error)

tests/include/init.vim
@@ -67,6 +67,7 @@ endfunction
 command! -nargs=+ NeomakeTestsWaitForMessage call s:wait_for_message(<args>)
 
 function! s:wait_for_finished_job()
+  Assert neomake#has_async_support(), 'NeomakeTestsWaitForNextFinishedJob should only be used for async mode'
   if !exists('#neomake_tests')
     call g:NeomakeSetupAutocmdWrappers()
   endif
@@ -320,6 +321,7 @@ let g:sleep_maker = NeomakeTestsCommandMaker('sleep-maker', 'sleep .05; echo sle
 let g:error_maker = NeomakeTestsCommandMaker('error-maker', 'echo error; false')
 let g:error_maker.errorformat = '%E%m'
 let g:success_maker = NeomakeTestsCommandMaker('success-maker', 'echo success')
+let g:true_maker = NeomakeTestsCommandMaker('true-maker', 'true')
 let g:doesnotexist_maker = {'exe': 'doesnotexist'}
 
 " A maker that generates incrementing errors.

tests/lists.vader
@@ -1,96 +1,196 @@
 Include: include/setup.vader
 
-Execute (Postprocessing should update list):
-  new
-  edit tests/fixtures/errors.sh
-  Save &filetype
-  set ft=sh
-  RunNeomake sh
-  let list = getloclist(0)
-  AssertNotEqual list, [], 'loclist is not empty'
+" " Execute ():
+" "   new
+" "   call setloclist(0, [])
+" " "   call setloclist(0, [{'lnum': 1, 'bufnr': 0, 'col': 0, 'vcol': 0, 'nr': -1, 'type': 'E', 'maker_name': 'unnamed_maker', 'text': 'error'}], 'r')
+" "
+" "   call setloclist(0, [{'lnum': 1, 'bufnr': 0, 'col': 0, 'valid': 0, 'vcol': 0, 'nr': -1, 'type': 'E', 'pattern': '', 'text': 'error1'}])
+" "   let c = winnr('$')
+" "   lwindow
+" "   AssertEqual c, winnr('$')
+" "   bwipe
+" "   Assert 0, 'end'
+"
+"
+" Execute (Postprocessing updates list):
+"   new
+"   edit tests/fixtures/errors.sh
+"   Save &filetype
+"   set ft=sh
+"   RunNeomake sh
+"   let list = getloclist(0)
+"   AssertNotEqual list, [], 'loclist is not empty'
+"
+"   let s:sh_maker = neomake#makers#ft#sh#sh()
+"   function Postprocess(entry)
+"     let a:entry.text .= ' SUFFIX'
+"   endfunction
+"   function! neomake#makers#ft#sh#sh()
+"     return extend(s:sh_maker, {'postprocess': function('Postprocess')})
+"   endfunction
+"   let expected_list = map(list, 'extend(v:val, {"text": v:val.text." SUFFIX"})')
+"   RunNeomake sh
+"   AssertEqual getloclist(0), expected_list
+"
+"   " Undo monkeypatching.
+"   runtime autoload/neomake/makers/ft/sh.vim
+"   delfunction Postprocess
+"   bwipe
+"
+" Execute (AddExprCallback with changed windows inbetween):
+"   if NeomakeAsyncTestsSetup()
+"     Save g:neomake_tests_postprocess_count
+"     let g:neomake_tests_postprocess_count = 0
+"     function! Postprocess(entry)
+"       let a:entry.text .= ' SUFFIX:'.(g:neomake_tests_postprocess_count/2)
+"       let g:neomake_tests_postprocess_count += 1
+"     endfunction
+"
+"     Save g:neomake_verbose
+"     " For the 'Modified list entry' debug message.
+"     let g:neomake_verbose = 3
+"
+"     let options = {
+"       \ 'postprocess': function('Postprocess'),
+"       \ 'buffer_output': 0,
+"       \ 'append_file': 0,
+"       \ }
+"     let maker_1 = neomake#utils#MakerFromCommand('echo 1a; sleep .1; echo 1b')
+"     call extend(maker_1, extend(copy(options), {'name': 'maker1'}))
+"     let maker_2 = neomake#utils#MakerFromCommand('echo 2')
+"     call extend(maker_2, extend(copy(options), {'name': 'maker2'}))
+"
+"     " Start 2 makers.
+"     call neomake#Make(1, [maker_1, maker_2])
+"     " Wait until partly finished.
+"     let maxwait = 50
+"     while g:neomake_tests_postprocess_count < 2 && maxwait
+"       sleep 10m
+"       let maxwait -= 1
+"     endwhile
+"     Assert maxwait > 0, 'postprocessing was not triggered'
+"     AssertEqual g:neomake_tests_postprocess_count, 2, 'postprocess count is != 2: '.g:neomake_tests_postprocess_count
+"     let loclist_texts = map(copy(getloclist(0)), 'v:val.text')
+"     AssertEqual sort(copy(loclist_texts)), ['1a SUFFIX:0', '2 SUFFIX:0']
+"
+"     " Start maker in new window (same winnr!)
+"     topleft new
+"     call neomake#Make(1, [maker_2])
+"     " Go to previous window, let previous job finish.
+"     wincmd j
+"     NeomakeTestsWaitForFinishedJobs
+"     AssertNeomakeMessage 'Output left to be processed, not cleaning job yet.'
+"     AssertEqual map(copy(getloclist(0)), 'v:val.text'),
+"       \ loclist_texts + ['1b SUFFIX:1']
+"     wincmd k
+"     AssertEqual map(copy(getloclist(0)), 'v:val.text'), ['2 SUFFIX:1']
+"     bwipe
+"     AssertNeomakeMessage "Modified list entry (postprocess): text: '2' => '2 SUFFIX:1'."
+"
+"     delfunction Postprocess
+"   endif
+"
+" Execute (Goes back to original window after opening list):
+"   Save g:neomake_open_list
+"   let g:neomake_open_list = 2
+"   new
+"   let winnr = winnr()
+"   let wincount = winnr('$')
+"   call neomake#Make(1, [g:error_maker])
+"   NeomakeTestsWaitForFinishedJobs
+"
+"   AssertEqual winnr, winnr()
+"   AssertEqual wincount + 1, winnr('$'), 'Location list appeared'
+"   AssertEqual map(copy(getloclist(0)), '[v:val.text, v:val.type]'), [['error', 'E']]
+"
+"   CallNeomake 1, [g:true_maker]
+"   AssertEqual getloclist(0), []
+"   AssertEqual wincount, winnr('$'), 'Location list was closed'
+"   bwipe
 
-  let s:sh_maker = neomake#makers#ft#sh#sh()
-  function Postprocess(entry)
-    let a:entry.text .= ' SUFFIX'
-  endfunction
-  function! neomake#makers#ft#sh#sh()
-    return extend(s:sh_maker, {'postprocess': function('Postprocess')})
-  endfunction
-  let expected_list = map(list, 'extend(v:val, {"text": v:val.text." SUFFIX"})')
-  RunNeomake sh
-  AssertEqual getloclist(0), expected_list
-
-  " Undo monkeypatching.
-  runtime autoload/neomake/makers/ft/sh.vim
-  delfunction Postprocess
-  bwipe
-
-Execute (AddExprCallback with changed windows inbetween):
-  if NeomakeAsyncTestsSetup()
-    Save g:neomake_tests_postprocess_count
-    let g:neomake_tests_postprocess_count = 0
-    function! Postprocess(entry)
-      let a:entry.text .= ' SUFFIX:'.(g:neomake_tests_postprocess_count/2)
-      let g:neomake_tests_postprocess_count += 1
-    endfunction
-
-    Save g:neomake_verbose
-    " For the 'Modified list entry' debug message.
-    let g:neomake_verbose = 3
-
-    let options = {
-      \ 'postprocess': function('Postprocess'),
-      \ 'buffer_output': 0,
-      \ 'append_file': 0,
-      \ }
-    let maker_1 = neomake#utils#MakerFromCommand('echo 1a; sleep .1; echo 1b')
-    call extend(maker_1, extend(copy(options), {'name': 'maker1'}))
-    let maker_2 = neomake#utils#MakerFromCommand('echo 2')
-    call extend(maker_2, extend(copy(options), {'name': 'maker2'}))
-
-    " Start 2 makers.
-    call neomake#Make(1, [maker_1, maker_2])
-    " Wait until partly finished.
-    let maxwait = 50
-    while g:neomake_tests_postprocess_count < 2 && maxwait
-      sleep 10m
-      let maxwait -= 1
-    endwhile
-    Assert maxwait > 0, 'postprocessing was not triggered'
-    AssertEqual g:neomake_tests_postprocess_count, 2, 'postprocess count is != 2: '.g:neomake_tests_postprocess_count
-    let loclist_texts = map(copy(getloclist(0)), 'v:val.text')
-    AssertEqual sort(copy(loclist_texts)), ['1a SUFFIX:0', '2 SUFFIX:0']
-
-    " Start maker in new window (same winnr!)
-    topleft new
-    call neomake#Make(1, [maker_2])
-    " Go to previous window, let previous job finish.
-    wincmd j
-    NeomakeTestsWaitForFinishedJobs
-    AssertNeomakeMessage 'Output left to be processed, not cleaning job yet.'
-    AssertEqual map(copy(getloclist(0)), 'v:val.text'),
-      \ loclist_texts + ['1b SUFFIX:1']
-    wincmd k
-    AssertEqual map(copy(getloclist(0)), 'v:val.text'), ['2 SUFFIX:1']
-    bwipe
-    AssertNeomakeMessage "Modified list entry (postprocess): text: '2' => '2 SUFFIX:1'."
-
-    delfunction Postprocess
-  endif
-
-Execute (Goes back to original window after opening list):
+Execute (open_list=2 handling with get_list_entries maker and invalid entries):
   Save g:neomake_open_list
   let g:neomake_open_list = 2
   new
   let winnr = winnr()
   let wincount = winnr('$')
-  call neomake#Make(1, [g:error_maker])
-  NeomakeTestsWaitForFinishedJobs
-  AssertEqual map(copy(getloclist(0)), '[v:val.text, v:val.type]'), [['error', 'E']]
+
+  let maker1 = {}
+  function! maker1.get_list_entries(...)
+    return [{'type': 'E', 'lnum': 1, 'text': 'error1'}]
+  endfunction
+  let maker2 = {}
+  function! maker2.get_list_entries(...)
+    return [{'type': 'E', 'lnum': 2, 'text': 'error2'}]
+  endfunction
+  CallNeomake 1, [maker1, maker2]
+  AssertEqual map(getloclist(0), '[v:val.text, v:val.type, v:val.valid]'), [
+  \ ['error1', 'E', 0],
+  \ ['error2', 'E', 0]]
+
+  AssertEqual winnr, winnr()
+  if has('patch-7.4.379')
+    AssertEqual wincount, winnr('$'), 'Location list did not appear for invalid entries'
+  else
+    AssertEqual wincount + 1, winnr('$'), 'Location list appeared for invalid entries (fixed in 7.4.379)'
+  endif
+
+  CallNeomake 1, [g:true_maker]
+  AssertEqual getloclist(0), []
+  bwipe
+
+Execute (open_list=2 handling with get_list_entries maker and invalid entries (qflist)):
+  Save g:neomake_open_list
+  let g:neomake_open_list = 2
+  new
+  let winnr = winnr()
+  let wincount = winnr('$')
+
+  let maker1 = {}
+  function! maker1.get_list_entries(...)
+    return [{'type': 'E', 'lnum': 1, 'text': 'error1'}]
+  endfunction
+  let maker2 = {}
+  function! maker2.get_list_entries(...)
+    return [{'type': 'E', 'lnum': 2, 'text': 'error2'}]
+  endfunction
+  CallNeomake 0, [maker1, maker2]
+  AssertEqual map(getqflist(), '[v:val.text, v:val.type, v:val.valid]'), [
+  \ ['error1', 'E', 0],
+  \ ['error2', 'E', 0]]
+
+  AssertEqual winnr, winnr()
+  if has('patch-7.4.379')
+    AssertEqual wincount, winnr('$'), 'Quickfix list did not appear for invalid entries'
+  else
+    AssertEqual wincount + 1, winnr('$'), 'Quickfix list appeared for invalid entries (fixed in 7.4.379)'
+  endif
+
+  CallNeomake 0, [g:true_maker]
+  AssertEqual getqflist(), []
+  bwipe
+
+Execute (open_list=2 handling with get_list_entries maker and valid entries):
+  Save g:neomake_open_list
+  let g:neomake_open_list = 2
+  new
+  let winnr = winnr()
+  let wincount = winnr('$')
+
+  let maker = {}
+  function! maker.get_list_entries(...)
+    return [{'type': 'E', 'lnum': 1, 'text': 'error', 'bufnr': bufnr('%')}]
+  endfunction
+  CallNeomake 1, [maker]
+  AssertEqual map(copy(getloclist(0)), '[v:val.text, v:val.type, v:val.valid]'), [['error', 'E', 1]]
 
   AssertEqual winnr, winnr()
   AssertEqual wincount + 1, winnr('$'), 'Location list appeared'
-  lclose
+
+  CallNeomake 1, [g:true_maker]
+  AssertEqual getloclist(0), []
+  AssertEqual wincount, winnr('$'), 'Location list was closed'
   bwipe
 
 Execute (Stays in location window with :lwindow in QuickFixCmdPost (like vim-qf)):
@@ -245,7 +345,7 @@ Execute (Goes back to original window after opening list (wincmd in autocmd)):
     AssertEqual wincount + 1, winnr('$'), 'Location list has appeared'
 
     Assert !exists('#neomake_event_queue#WinEnter'), 'autocmd was deleted'
-    AssertNeomakeMessage 'Opening location list.'
+    AssertNeomakeMessage 'Handling location list: executing lwindow.'
     AssertNeomakeMessage 'action queue: processed 1 items.'
 
     wincmd p
@@ -256,7 +356,7 @@ Execute (Goes back to original window after opening list (wincmd in autocmd)):
     bwipe
   endif
 
-Execute (open_list=1: just opens location window):
+Execute (open_list=1 with job: just opens location window):
   Save g:neomake_open_list
   let g:neomake_open_list = 1
   new
@@ -270,9 +370,16 @@ Execute (open_list=1: just opens location window):
   AssertEqual wincount + 1, winnr('$'), 'Location list has appeared'
 
   Assert !exists('#neomake_event_queue#WinEnter'), 'autocmd was not setup'
-  AssertNeomakeMessage 'Opening location list.'
+  AssertNeomakeMessage 'Handling location list: executing lwindow.'
 
-  lclose
+  AssertEqual winnr(), winnr('$'), 'In location window.'
+  wincmd p
+
+  call neomake#Make(1, [g:true_maker])
+  NeomakeTestsWaitForFinishedJobs
+  AssertEqual wincount, winnr('$'), 'Location list has been closed'
+  exe winnr 'wincmd w'
+
   bwipe
   bwipe
 
@@ -295,7 +402,7 @@ Execute (open_list=0: does not open location window):
   bwipe
 
 Execute (open_list=1 with height=0: does not open location window):
-  Save g:neomake_open_list
+  Save g:neomake_open_list, g:neomake_list_height
   let g:neomake_open_list = 1
   let g:neomake_list_height = 0
   new
@@ -308,4 +415,34 @@ Execute (open_list=1 with height=0: does not open location window):
 
   Assert !exists('#neomake_event_queue#WinEnter'), 'autocmd was not setup'
   Assert !exists('#neomake_event_queue'), 'augroup does not exist'
+  bwipe
+
+Execute (location list gets created with entries from first job, closed only after all jobs finished):
+  Save g:neomake_open_list
+  let g:neomake_open_list = 2
+  new
+  let wincount = winnr('$')
+
+  let b:neomake_serialize = 1
+  call neomake#Make(1, [g:error_maker, g:true_maker])
+  if neomake#has_async_support()
+    NeomakeTestsWaitForNextFinishedJob
+    AssertEqual map(getloclist(0), '[v:val.text, v:val.type, v:val.valid]'), [['error', 'E', 1]]
+    AssertEqual wincount + 1, winnr('$'), 'Location list has appeared'
+    NeomakeTestsWaitForFinishedJobs
+  endif
+  AssertEqual map(getloclist(0), '[v:val.text, v:val.type, v:val.valid]'), [['error', 'E', 1]]
+  AssertEqual wincount + 1, winnr('$'), 'Location list has appeared'
+
+  let maker1 = NeomakeTestsCommandMaker('maker1', 'sleep .1')
+  let maker2 = NeomakeTestsCommandMaker('maker2', 'true')
+  let b:neomake_serialize = 1
+  call neomake#Make(1, [maker1, maker2])
+  if neomake#has_async_support()
+    NeomakeTestsWaitForNextFinishedJob
+    AssertEqual map(getloclist(0), '[v:val.text, v:val.type, v:val.valid]'), [['error', 'E', 1]]
+    NeomakeTestsWaitForFinishedJobs
+  endif
+  AssertEqual getloclist(0), []
+  AssertEqual wincount, winnr('$'), 'Location list was closed'
   bwipe