b16813c1bab582473b2ef946a33084f8683c2523
Daniel Hahler <git@thequod.de>
2018-08-18 15:50:12 +0200

    tests/helpers/trap.sh

--- tests/helpers/trap.sh
+++ tests/helpers/trap.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # A test script that ignores SIGTERM.
+
+set -x
 
 # XXX: does not work on Windows (Neovim, does not appear to trap).
 # Started via "cmd.exe /s /c C:\projects\neomake\tests/helpers/trap.sh".
 trap 'echo not stopping on SIGTERM' TERM
+trap '' INT
 # trap 'echo stopping on SIGHUP; exit' HUP
 
 echo "SHELL: $SHELL"
