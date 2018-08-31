neomake/neomake

commit 421af22b920d2e0e917dc04134c5254e22b00833

Improve NeomakeInfo, refactor maker instantiation
- support display of info for single maker, e.g. `:NeomakeInfo python`
- fixes for &makeprg handling
- neomake#utils#MakerFromCommand: set exe/args directly
  This allows for a makeprg maker to have the expected `exe`, and setting
  exe/args does not require jobinfo to be there already.
- refactor/split neomake#GetMaker

Fixes https://github.com/neomake/neomake/issues/1615.
Fixes https://github.com/neomake/neomake/issues/1505.
Closes https://github.com/neomake/neomake/pull/1954.

autoload/neomake.vim
@@ -523,7 +523,6 @@ if !s:can_use_env_in_job_opts
     endfunction
 endif
 
-let s:maker_base = {}
 let s:command_maker_base = copy(g:neomake#core#command_maker_base)
 " Check if a temporary file is used, and set it in s:make_info in case it is.
 function! s:command_maker_base._get_tempfilename(jobinfo) abort dict
@@ -706,6 +705,7 @@ function! s:command_maker_base._bind_args() abort dict
         let args = copy(self.args)
     endif
     let self.args = args
+    return self
 endfunction
 
 function! s:command_maker_base._get_argv(jobinfo) abort dict
@@ -737,54 +737,79 @@ function! s:GetMakerForFiletype(ft, maker_name) abort
     return s:unset_dict
 endfunction
 
+function! neomake#get_maker_by_name(maker_name, ...) abort
+    let for_ft = a:0 ? a:1 : 0
+    let ft_config = for_ft is# 0 ? &filetype : for_ft
+    let bufnr = bufnr('%')
+    if a:maker_name !~# '\v^\w+$'
+        throw printf('Neomake: Invalid maker name: "%s"', a:maker_name)
+    endif
+
+    let maker = neomake#utils#GetSetting('maker', {'name': a:maker_name}, s:unset_dict, ft_config, bufnr)
+    if maker is# s:unset_dict
+        if a:maker_name ==# 'makeprg'
+            let maker = s:get_makeprg_maker()
+        elseif for_ft isnot# 0
+            let maker = s:GetMakerForFiletype(for_ft, a:maker_name)
+        else
+            call neomake#utils#load_global_makers()
+            let f = 'neomake#makers#'.a:maker_name.'#'.a:maker_name
+            if exists('*'.f)
+                let maker = call(f, [])
+            endif
+        endif
+    endif
+    if type(maker) != type({})
+        throw printf('Neomake: Got non-dict for maker %s: %s',
+                    \ a:maker_name, maker)
+    endif
+    if maker isnot# s:unset_dict && !has_key(maker, 'name')
+        let maker.name = a:maker_name
+    endif
+    return maker
+endfunction
+
 function! neomake#GetMaker(name_or_maker, ...) abort
-    let ft = a:0 ? a:1 : &filetype
+    let for_ft = a:0 ? a:1 : 0
     let bufnr = bufnr('%')
     if type(a:name_or_maker) == type({})
         let maker = a:name_or_maker
-    elseif a:name_or_maker ==# 'makeprg'
-        let maker = s:get_makeprg_maker()
-    elseif a:name_or_maker !~# '\v^\w+$'
-        throw printf('Neomake: Invalid maker name: "%s"', a:name_or_maker)
+        if !has_key(maker, 'name')
+            let maker.name = 'unnamed_maker'
+        endif
     else
-        let maker = neomake#utils#GetSetting('maker', {'name': a:name_or_maker}, s:unset_dict, ft, bufnr)
+        let maker = neomake#get_maker_by_name(a:name_or_maker, for_ft)
         if maker is# s:unset_dict
-            if !empty(ft)
-                let maker = s:GetMakerForFiletype(ft, a:name_or_maker)
-            endif
-            if maker is# s:unset_dict
-                call neomake#utils#load_global_makers()
-                let f = 'neomake#makers#'.a:name_or_maker.'#'.a:name_or_maker
-                if exists('*'.f)
-                    let maker = call(f, [])
-                endif
-                if maker is# s:unset_dict
-                    throw printf('Neomake: Maker not found (for %s): %s',
-                                \ !empty(ft) ? 'filetype '.ft : 'empty filetype',
-                                \ a:name_or_maker)
-                endif
+            if !a:0
+                " Check &filetype if no args where provided.
+                let maker = neomake#get_maker_by_name(a:name_or_maker, &filetype)
             endif
         endif
-        if type(maker) != type({})
-            throw printf('Neomake: Got non-dict for maker %s: %s',
-                        \ a:name_or_maker, maker)
+        if maker is# s:unset_dict
+            if for_ft isnot# 0
+                throw printf('Neomake: Maker not found (for %s): %s',
+                            \ !empty(for_ft) ? 'filetype '.for_ft : 'empty filetype',
+                            \ a:name_or_maker)
+            else
+                throw printf('Neomake: Maker not found (without filetype): %s',
+                            \ a:name_or_maker)
+            endif
         endif
     endif
+    return neomake#create_maker_object(maker, a:0 ? a:1 : &filetype)
+endfunction
+
+" NOTE: uses ft and bufnr for config only.
+function! neomake#create_maker_object(maker, ft) abort
+    let [maker, ft, bufnr] = [a:maker, a:ft, bufnr('%')]
 
     " Create the maker object.
     let GetEntries = neomake#utils#GetSetting('get_list_entries', maker, -1, ft, bufnr)
     if GetEntries isnot# -1
-        let maker = extend(copy(s:maker_base), copy(maker))
+        let maker = copy(maker)
         let maker.get_list_entries = GetEntries
     else
         let maker = extend(copy(s:command_maker_base), copy(maker))
-    endif
-    if !has_key(maker, 'name')
-        if type(a:name_or_maker) == type('')
-            let maker.name = a:name_or_maker
-        else
-            let maker.name = 'unnamed_maker'
-        endif
     endif
     if !has_key(maker, 'get_list_entries')
         " Set defaults for command/job based makers.
@@ -898,8 +923,17 @@ function! neomake#GetEnabledMakers(...) abort
         " This variable is also used for project jobs, so it has no
         " buffer local ('b:') counterpart for now.
         let enabled_makers = copy(get(g:, 'neomake_enabled_makers', []))
-        call map(enabled_makers, "extend(neomake#GetMaker(v:val),
-                    \ {'auto_enabled': 0}, 'error')")
+        if empty(enabled_makers)
+            let makeprg_maker = s:get_makeprg_maker()
+            if !empty(makeprg_maker)
+                let makeprg_maker = neomake#GetMaker(makeprg_maker)
+                let makeprg_maker.auto_enabled = 1
+                let enabled_makers = [makeprg_maker]
+            endif
+        else
+            call map(enabled_makers, "extend(neomake#GetMaker(v:val),
+                        \ {'auto_enabled': 0}, 'error')")
+        endif
     else
         let enabled_makers = []
         let makers = neomake#utils#GetSetting('enabled_makers', {}, s:unset_list, a:1, bufnr('%'))
@@ -1197,8 +1231,16 @@ endfunction
 " This could be cached, but needs to take into account / set &errorformat,
 " and other settings that are handled by neomake#GetMaker.
 function! s:get_makeprg_maker() abort
-    let maker = neomake#utils#MakerFromCommand(&makeprg)
+    if empty(&makeprg)
+        return {}
+    elseif &makeprg =~# '\s'
+        let maker = neomake#utils#MakerFromCommand(&makeprg)
+    else
+        let maker = neomake#utils#MakerFromCommand([&makeprg])
+    endif
     let maker.name = 'makeprg'
+    " Do not append file.  &makeprg should contain %/# for this instead.
+    let maker.append_file = 0
     return neomake#GetMaker(maker)
 endfunction
 
@@ -1269,8 +1311,6 @@ function! s:Make(options) abort
                     endif
                     unlet s:make_info[make_id]
                     return []
-                else
-                    let makers = [s:get_makeprg_maker()]
                 endif
             endif
         endif
@@ -1360,7 +1400,7 @@ function! s:AddExprCallback(jobinfo, prev_list) abort
         let postprocessors = Postprocess
     endif
     let debug = neomake#utils#get_verbosity(a:jobinfo) >= 3 || !empty(get(g:, 'neomake_logfile'))
-    let maker_name = get(maker, 'name', 'makeprg')
+    let maker_name = maker.name
     let make_info = s:make_info[a:jobinfo.make_id]
     let default_type = 'unset'
 

tests/errors.vader
@@ -54,6 +54,8 @@ Execute (Error with non-existing filename for 2nd maker):
   bwipe!
 
 Execute (maker.exe gets checked):
+  call NeomakeTestsSetVimMessagesMarker()
+
   let maker = {'exe': 'doesnotexist'}
   let maker_non_string_exe = {}
   function maker_non_string_exe.exe()
@@ -65,16 +67,29 @@ Execute (maker.exe gets checked):
   AssertNeomakeMessage 'Exe (doesnotexist) of maker unnamed_maker is not executable.', 0, log_context
   AssertNeomakeMessage 'Non-string given for executable of maker unnamed_maker: type 2.', 0, log_context
   AssertNeomakeMessage 'Nothing to make: no valid makers.'
+  AssertEqual len(g:neomake_test_messages), 4
 
+  " Is still an error on second invocation (not autoconfigured).
   CallNeomake 1, [maker, maker_non_string_exe]
   let make_id = neomake#GetStatus().last_make_id
-
   let log_context = {'make_id': neomake#GetStatus().last_make_id, 'bufnr': bufnr('%')}
-  " Should get logged as debug message on the second error.
-  AssertNeomakeMessage 'Exe (doesnotexist) of maker unnamed_maker is not executable.', 3, log_context
+  AssertNeomakeMessage 'Exe (doesnotexist) of maker unnamed_maker is not executable.', 0, log_context
   AssertNeomakeMessage 'Non-string given for executable of maker unnamed_maker: type 2.', 0, log_context
   AssertNeomakeMessage 'Nothing to make: no valid makers.', 3, log_context
-  Assert len(g:neomake_test_messages), 4
+  AssertEqual len(g:neomake_test_messages), 8
+
+  " Error gets logged as debug message if auto-enabled.
+  let maker.auto_enabled = 1
+  let maker_non_string_exe.auto_enabled = 1
+  CallNeomake 1, [maker, maker_non_string_exe]
+  let make_id = neomake#GetStatus().last_make_id
+  let log_context = {'make_id': neomake#GetStatus().last_make_id, 'bufnr': bufnr('%')}
+  AssertNeomakeMessage 'Exe (doesnotexist) of auto-configured maker unnamed_maker is not executable, skipping.', 3, log_context
+  AssertNeomakeMessage 'Non-string given for executable of maker unnamed_maker: type 2.', 3, log_context
+  AssertNeomakeMessage 'Nothing to make: no valid makers.', 3, log_context
+  AssertEqual len(g:neomake_test_messages), 12
+
+  AssertEqual NeomakeTestsGetVimMessages(), []
 
 Execute (maker.exe gets checked (auto-enabled)):
   new
@@ -95,7 +110,11 @@ Execute (neomake#GetMaker with non-existent maker throws errors):
   AssertThrows call neomake#GetMaker('non-existent')
   AssertEqual g:vader_exception, 'Neomake: Invalid maker name: "non-existent"'
   AssertThrows call neomake#GetMaker('nonexistent')
+  AssertEqual g:vader_exception, 'Neomake: Maker not found (without filetype): nonexistent'
+  AssertThrows call neomake#GetMaker('nonexistent', '')
   AssertEqual g:vader_exception, 'Neomake: Maker not found (for empty filetype): nonexistent'
+  AssertThrows call neomake#GetMaker('nonexistent', 'c')
+  AssertEqual g:vader_exception, 'Neomake: Maker not found (for filetype c): nonexistent'
   AssertEqual len(g:neomake_test_messages), 0
 
 Execute (Errors from neomake#GetMaker get handled by neomake#Make):

plugin/neomake.vim
@@ -19,7 +19,8 @@ command! -bang -nargs=1 -complete=custom,neomake#cmd#complete_jobs
             \ NeomakeCancelJob call neomake#CancelJob(<q-args>, <bang>0)
 command! -bang NeomakeCancelJobs call neomake#CancelJobs(<bang>0)
 
-command! -bang -bar NeomakeInfo call neomake#debug#display_info(<bang>0)
+command! -bang -bar -nargs=? -complete=customlist,neomake#cmd#complete_makers
+            \ NeomakeInfo call neomake#debug#display_info(<bang>0, <f-args>)
 
 " Enable/disable/toggle commands.  {{{
 function! s:handle_disabled_status(scope, disabled, verbose) abort

autoload/neomake/core.vim
@@ -1,12 +1,48 @@
 let g:neomake#core#valid_maker_name_pattern = '\v^\w+$'
 
-" Keep track of for what maker.exe an error was thrown.
-let s:exe_error_thrown = {}
-
 function! neomake#core#create_jobs(options, makers) abort
     let args = [a:options, a:makers]
     let jobs = call('s:bind_makers_for_job', args)
     return jobs
+endfunction
+
+function! neomake#core#instantiate_maker(maker, options, check_exe) abort
+    let maker = a:maker
+    let options = a:options
+    " Call .fn function in maker object, if any.
+    if has_key(maker, 'fn')
+        " TODO: Allow to throw and/or return 0 to abort/skip?!
+        let returned_maker = call(maker.fn, [options], maker)
+        if returned_maker isnot# 0
+            " This conditional assignment allows to both return a copy
+            " (factory), while also can be used as a init method.
+            let maker = returned_maker
+        endif
+    endif
+
+    if has_key(maker, '_bind_args')
+        call maker._bind_args()
+        if type(maker.exe) != type('')
+            let error = printf('Non-string given for executable of maker %s: type %s',
+                        \ maker.name, type(maker.exe))
+            if !get(maker, 'auto_enabled', 0)
+                throw 'Neomake: '.error
+            endif
+            call neomake#log#debug(error.'.', options)
+            return {}
+        endif
+        if a:check_exe && !executable(maker.exe)
+            if get(maker, 'auto_enabled', 0)
+                call neomake#log#debug(printf(
+                            \ 'Exe (%s) of auto-configured maker %s is not executable, skipping.', maker.exe, maker.name), options)
+            else
+                let error = printf('Exe (%s) of maker %s is not executable', maker.exe, maker.name)
+                throw 'Neomake: '.error
+            endif
+            return {}
+        endif
+    endif
+    return maker
 endfunction
 
 " Map/bind a:makers to a list of job options, using a:options.
@@ -15,53 +51,16 @@ function! s:bind_makers_for_job(options, makers) abort
     for maker in a:makers
         let options = copy(a:options)
         try
-            " Call .fn function in maker object, if any.
-            if has_key(maker, 'fn')
-                " TODO: Allow to throw and/or return 0 to abort/skip?!
-                let returned_maker = call(maker.fn, [options], maker)
-                if returned_maker isnot# 0
-                    " This conditional assignment allows to both return a copy
-                    " (factory), while also can be used as a init method.
-                    let maker = returned_maker
-                endif
-            endif
-
-            if has_key(maker, '_bind_args')
-                call maker._bind_args()
-                if type(maker.exe) != type('')
-                    let error = printf('Non-string given for executable of maker %s: type %s.',
-                                \ maker.name, type(maker.exe))
-                    if !get(maker, 'auto_enabled', 0)
-                        call neomake#log#error(error, options)
-                    else
-                        call neomake#log#debug(error, options)
-                    endif
-                    continue
-                endif
-                if !executable(maker.exe)
-                    if !get(maker, 'auto_enabled', 0)
-                        let error = printf('Exe (%s) of maker %s is not executable.', maker.exe, maker.name)
-                        if !has_key(s:exe_error_thrown, maker.exe)
-                            let s:exe_error_thrown[maker.exe] = 1
-                            call neomake#log#error(error, options)
-                        else
-                            call neomake#log#debug(error, options)
-                        endif
-                    else
-                        call neomake#log#debug(printf(
-                                    \ 'Exe (%s) of auto-configured maker %s is not executable, skipping.', maker.exe, maker.name), options)
-                    endif
-                    continue
-                endif
-            endif
-
+            let maker = neomake#core#instantiate_maker(maker, options, 1)
         catch /^Neomake: /
             let error = substitute(v:exception, '^Neomake: ', '', '').'.'
-            call neomake#log#error(error, {'make_id': options.make_id})
+            call neomake#log#error(error, options)
             continue
         endtry
-        let options.maker = maker
-        let r += [options]
+        if !empty(maker)
+            let options.maker = maker
+            let r += [options]
+        endif
     endfor
     return r
 endfunction

autoload/neomake/debug.vim
@@ -56,10 +56,15 @@ function! neomake#debug#validate_maker(maker) abort
         endif
     endfor
 
-    if !executable(a:maker.exe)
-        let t = get(a:maker, 'auto_enabled', 0) ? 'warnings' : 'errors'
-        let issues[t] += [printf("maker's exe (%s) is not executable.", a:maker.exe)]
-    endif
+    try
+        let maker = neomake#core#instantiate_maker(a:maker, {}, 0)
+        if !executable(maker.exe)
+            let t = get(maker, 'auto_enabled', 0) ? 'warnings' : 'errors'
+            let issues[t] += [printf("maker's exe (%s) is not executable.", maker.exe)]
+        endif
+    catch /^Neomake: /
+        let issues.errors += [substitute(v:exception, '^Neomake: ', '', '').'.']
+    endtry
 
     if has_key(a:maker, 'name')
         if a:maker.name !~# g:neomake#core#valid_maker_name_pattern
@@ -74,7 +79,7 @@ function! neomake#debug#validate_maker(maker) abort
 endfunction
 
 " Optional arg: ft
-function! s:get_maker_info(...) abort
+function! s:get_makers_info(...) abort
     let maker_names = call('neomake#GetEnabledMakers', a:000)
     if empty(maker_names)
         return ['None.']
@@ -84,36 +89,97 @@ function! s:get_maker_info(...) abort
     for maker_name in maker_names
         let maker = call('neomake#GetMaker', [maker_name] + a:000)
         let r += [' - '.maker.name]
-        for [k, V] in sort(copy(items(maker)))
-            if k !=# 'name' && k !=# 'ft' && k !~# '^_'
-                if !has_key(maker_defaults, k)
-                            \ || type(V) != type(maker_defaults[k])
-                            \ || V !=# maker_defaults[k]
-                    let r += ['   - '.k.': '.string(V)]
-                endif
-            endif
-            unlet V  " vim73
-        endfor
-
-        let issues = neomake#debug#validate_maker(maker)
-        if !empty(issues)
-            for type in sort(copy(keys(issues)))
-                let items = issues[type]
-                if !empty(items)
-                    let r += ['   - '.toupper(type) . ':']
-                    for issue in items
-                        let r += ['     - ' . issue]
-                    endfor
-                endif
-            endfor
-        endif
+        let r += map(s:get_maker_info(maker, maker_defaults), "'  '.v:val")
     endfor
     return r
 endfunction
 
+function! s:get_maker_info(maker, ...) abort
+    let maker_defaults = a:0 ? a:1 : {}
+    let maker = a:maker
+    let r = []
+    for [k, V] in sort(copy(items(maker)))
+        if k !=# 'name' && k !=# 'ft' && k !~# '^_'
+            if !has_key(maker_defaults, k)
+                        \ || type(V) != type(maker_defaults[k])
+                        \ || V !=# maker_defaults[k]
+                let r += [' - '.k.': '.string(V)]
+            endif
+        endif
+        unlet V  " vim73
+    endfor
+
+    let issues = neomake#debug#validate_maker(maker)
+    if !empty(issues)
+        for type in sort(copy(keys(issues)))
+            let items = issues[type]
+            if !empty(items)
+                let r += [' - '.toupper(type) . ':']
+                for issue in items
+                    let r += ['   - ' . issue]
+                endfor
+            endif
+        endfor
+    endif
+
+    if type(maker.exe) == type('') && executable(maker.exe)
+        let version_arg = get(maker, 'version_arg', '--version')
+        let exe = exists('*exepath') ? exepath(maker.exe) : maker.exe
+        let version_output = neomake#compat#systemlist([exe, version_arg])
+        if empty(version_output)
+            let version_output = [printf(
+                        \ 'failed to get version information (%s)',
+                        \ v:shell_error)]
+        endif
+        let r += [printf(' - version information (%s %s): %s',
+                    \ exe,
+                    \ version_arg,
+                    \ join(version_output, "\n     "))]
+    endif
+    return r
+endfunction
+
+function! s:get_fts_with_makers() abort
+    return neomake#compat#uniq(sort(map(split(globpath(escape(&runtimepath, ' '),
+          \ 'autoload/neomake/makers/ft/*.vim'), "\n"),
+          \ 'fnamemodify(v:val, ":t:r")')))
+endfunction
+
+function! neomake#debug#get_maker_info(maker_name) abort
+    let source = ''
+    let maker = neomake#get_maker_by_name(a:maker_name, &filetype)
+    if empty(maker)
+        let maker = neomake#get_maker_by_name(a:maker_name)
+        if empty(maker)
+            let fts = filter(s:get_fts_with_makers(), 'v:val != &filetype')
+            for ft in fts
+                let maker = neomake#get_maker_by_name(a:maker_name, ft)
+                if !empty(maker)
+                    let source = 'filetype '.ft
+                    break
+                endif
+            endfor
+        else
+            let source = 'project maker'
+        endif
+    endif
+    if empty(maker)
+        call neomake#log#error(printf('Maker not found: %s.', a:maker_name))
+        return []
+    endif
+    let maker = neomake#create_maker_object(maker, &filetype)
+    return [maker.name . (empty(source) ? '' : ' ('.source.')')]
+                \ + s:get_maker_info(maker)
+endfunction
+
 function! neomake#debug#display_info(...) abort
     let bang = a:0 ? a:1 : 0
-    let lines = neomake#debug#_get_info_lines()
+    if a:0 > 1
+        let maker_name = a:2
+        let lines = neomake#debug#get_maker_info(maker_name)
+    else
+        let lines = neomake#debug#_get_info_lines()
+    endif
     if bang
         try
             call setreg('+', join(lines, "\n"), 'l')
@@ -145,7 +211,7 @@ function! neomake#debug#_get_info_lines() abort
     let r += ['##### Enabled makers']
     let r += ['']
     let r += ['For the current filetype ("'.ft.'", used with :Neomake):']
-    let r += s:get_maker_info(ft)
+    let r += s:get_makers_info(ft)
     if empty(ft)
         let r += ['NOTE: the current buffer does not have a filetype.']
     else
@@ -154,15 +220,15 @@ function! neomake#debug#_get_info_lines() abort
                     \ .' to configure it (or b:neomake_'.conf_ft.'_enabled_makers).']
     endif
     let r += ['']
+    let r += ['For the project (used with :Neomake!):']
+    let r += s:get_makers_info()
+    let r += ['NOTE: you can define g:neomake_enabled_makers to configure it.']
+    let r += ['']
     let r += ['Default maker settings:']
     for [k, v] in items(neomake#config#get('maker_defaults'))
         let r += [' - '.k.': '.string(v)]
         unlet! v  " Fix variable type mismatch with Vim 7.3.
     endfor
-    let r += ['']
-    let r += ['For the project (used with :Neomake!):']
-    let r += s:get_maker_info()
-    let r += ['NOTE: you can define g:neomake_enabled_makers to configure it.']
     let r += ['']
     let r += ['##### Settings']
     let r += ['']

autoload/neomake/utils.vim
@@ -78,29 +78,9 @@ function! neomake#utils#Exists(exe) abort
 endfunction
 
 " Object used with neomake#utils#MakerFromCommand.
-" It creates args in `.fn` and handles appending the filename according to
-" if it was created from a string or list of args.
 let s:maker_from_command = extend(copy(g:neomake#core#command_maker_base), {
             \ 'remove_invalid_entries': 0,
             \ })
-function! s:maker_from_command.fn(_options) dict abort
-    " Return a cleaned up copy of self.
-    let maker = filter(deepcopy(self), "v:key !~# '^__' && v:key !=# 'fn'")
-
-    let command = self.__command
-    if type(command) == type('')
-        let argv = split(&shell) + split(&shellcmdflag)
-        let maker.exe = argv[0]
-        let maker.args = argv[1:] + [command]
-        let maker.__command_is_string = 1
-    else
-        let maker.exe = command[0]
-        let maker.args = command[1:]
-        let maker.__command_is_string = 0
-    endif
-    return maker
-endfunction
-
 function! s:maker_from_command._get_argv(jobinfo) abort dict
     let fname = self._get_fname_for_args(a:jobinfo)
     let args = neomake#utils#ExpandArgs(self.args)
@@ -115,12 +95,21 @@ function! s:maker_from_command._get_argv(jobinfo) abort dict
     return neomake#compat#get_argv(self.exe, args, 1)
 endfunction
 
-" Create a maker object, with a "fn" callback.
+" Create a maker object for a given command.
 " Args: command (string or list).  Gets wrapped in a shell in case it is a
 "       string.
 function! neomake#utils#MakerFromCommand(command) abort
     let maker = copy(s:maker_from_command)
-    let maker.__command = a:command
+    if type(a:command) == type('')
+        let argv = split(&shell) + split(&shellcmdflag)
+        let maker.exe = argv[0]
+        let maker.args = argv[1:] + [a:command]
+        let maker.__command_is_string = 1
+    else
+        let maker.exe = a:command[0]
+        let maker.args = a:command[1:]
+        let maker.__command_is_string = 0
+    endif
     return maker
 endfunction
 

tests/cmd_neomakeinfo.vader
@@ -187,3 +187,59 @@ Execute (Verbose NeomakeInfo does not display message for filetype with no maker
 
   AssertEqual msgs, []
   Assert index(info, 'For the current filetype ("doesnotexist", used with :Neomake):') > 0
+
+Execute (NeomakeInfo with maker name):
+  " NOTE: Vim needs non-empty PATH for executable to be false!
+  " (https://github.com/vim/vim/pull/3283)
+  call g:NeomakeTestsSetPATH('/doesnotexist')
+  Assert !executable('make')
+
+  new
+  noautocmd set filetype=neomake_tests
+  NeomakeInfo foo
+  AssertNeomakeMessage 'Maker not found: foo.', 0
+
+  call NeomakeTestsSetVimMessagesMarker()
+  NeomakeInfo makeprg
+  AssertEqual NeomakeTestsGetVimMessages(), []
+  AssertEqual neomake#debug#get_maker_info('maker_with_nonstring_exe')[-2:], [
+  \ ' - ERRORS:',
+  \ "   - Non-string given for executable of maker maker_with_nonstring_exe: type 2.",
+  \ ]
+
+  call NeomakeTestsSetVimMessagesMarker()
+  NeomakeInfo makeprg
+  AssertEqual NeomakeTestsGetVimMessages(), []
+  AssertEqual neomake#debug#get_maker_info('makeprg')[-2:], [
+  \ ' - ERRORS:',
+  \ "   - maker's exe (make) is not executable.",
+  \ ]
+
+  Save &makeprg
+  set makeprg=custommakeprg
+  NeomakeInfo makeprg
+  AssertEqual NeomakeTestsGetVimMessages(), []
+  AssertEqual neomake#debug#get_maker_info('makeprg')[-2:], [
+  \ ' - ERRORS:',
+  \ "   - maker's exe (custommakeprg) is not executable.",
+  \ ]
+
+  " Uses &shell when checking executable for makeprg with spaces.
+  Save &shell
+  set shell=customshell
+  set makeprg=custom\ make\ prg
+  NeomakeInfo makeprg
+  AssertEqual NeomakeTestsGetVimMessages(), []
+  AssertEqual neomake#debug#get_maker_info('makeprg')[-2:], [
+  \ ' - ERRORS:',
+  \ "   - maker's exe (customshell) is not executable.",
+  \ ]
+
+  let b:neomake_makeprg_exe = 'custom_exe'
+  NeomakeInfo makeprg
+  AssertEqual NeomakeTestsGetVimMessages(), []
+  AssertEqual neomake#debug#get_maker_info('makeprg')[-2:], [
+  \ ' - ERRORS:',
+  \ "   - maker's exe (custom_exe) is not executable.",
+  \ ]
+  bwipe

tests/completion.vader
@@ -9,6 +9,7 @@ Execute (neomake#cmd#complete_makers: completes makers for b:):
     \ }
   set filetype=c
   call g:NeomakeTestsCreateExe('lint', [])
+  AssertEqual neomake#GetMaker('checkpatch').name, 'checkpatch'
   Assert index(neomake#cmd#complete_makers('', 'Neomake '), 'lint') != -1, 'lint maker gets completed'
   bwipe
 

commit 646bbe75d1f94dcb3dc633fd6b14bee1cb8ff35d

Fix `:Neomake! maker`
It was broken since 421af22.

autoload/neomake.vim
@@ -1303,7 +1303,7 @@ function! s:Make(options) abort
         unlet options.jobs
     else
         if has_key(options, 'enabled_makers')
-            let makers = neomake#map_makers(options.enabled_makers, options.ft, 0)
+            let makers = neomake#map_makers(options.enabled_makers, file_mode ? options.ft : -1, 0)
             unlet options.enabled_makers
         else
             let makers = call('neomake#GetEnabledMakers', file_mode ? [options.ft] : [])
@@ -2833,9 +2833,10 @@ endfunction
 function! neomake#map_makers(makers, ft, auto_enabled) abort
     let makers = []
     let errors = []
+    let get_args = a:ft is# -1 ? [] : [a:ft]
     for maker in a:makers
         try
-            let m = neomake#GetMaker(maker, a:ft)
+            let m = call('neomake#GetMaker', [maker] + get_args)
         catch /^Neomake: /
             call add(errors, substitute(v:exception, '^Neomake: ', '', '').'.')
             continue

tests/errors.vader
@@ -121,7 +121,7 @@ Execute (Errors from neomake#GetMaker get handled by neomake#Make):
   CallNeomake 1, ['g:invalid-name']
   AssertNeomakeMessage 'Invalid maker name: "g:invalid-name".', 0
   CallNeomake 0, ['nonexistent']
-  AssertNeomakeMessage 'Maker not found (for empty filetype): nonexistent.', 0
+  AssertNeomakeMessage 'Maker not found (without filetype): nonexistent.', 0
 
 Execute (Error from neomake#GetMaker for non-dict maker):
   new

tests/integration.vader
@@ -969,3 +969,8 @@ Execute (neomake#GetCurrentErrorMsg):
   normal! l
   AssertEqual neomake#GetCurrentErrorMsg(), 'my_maker: error 3.2 (0)'
   bwipe!
+
+Execute (Neomake! clippy):
+  call g:NeomakeTestsCreateExe('clippy', [])
+  RunNeomakeProject clippy
+  AssertNeomakeMessage 'Running makers: clippy.', 3